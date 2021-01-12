707d22b93acc3a7a61eab51a9e217e2c794314be
Ran Wei <harryweebb@gmail.com>
2021-01-11 20:55:03 -0700

    Add data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,4 +4,11 @@ var tableData = data;
 // YOUR CODE HERE!
 
 var tbody = d3.select("tbody");
-console.log(data);
+
+data.forEach((sightings) => {
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key,value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
